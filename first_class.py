1640f085dddcd997e1135385cee0235486e9a084
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2023-05-05 08:23:37 +0200

    Add files via upload

---
+++ first_class.py
@@ -0,0 +1,38 @@
+# Dane kontaktowe:
+# Szymon Chlebowski, szymon.chlebowski@amu.edu.pl lub MsTeams chat
+# Dyżury (pokój 97, budynek AB) 
+# wtorek 8.30-9.30, środa 11.15-12.15 (po uprzednim umówieniu się)
+
+# 30 punktów: kolokwium (indywidualne przy komputerze), 
+# 20 punktów: indywidualne wejściówki/wyjściówki i/lub praca w małych grupach.
+
+# kod zajęć będzie dostępny na Githubie a link będzie udostepniany na stronie:
+# http://sc52172.home.amu.edu.pl/
+
+# Temat pierwszych zajęć: 
+# - rozgrzewka;
+# - rekurenjca, rekurencja ogonowa, iteracja
+
+# 1a. Zdefiniuj funkcję silnia, używając standardowej rekurencji.
+
+# 1b. Zdefiniuj funkcję silnia, używając rekurencji ogonowej.
+
+# 1c. Zdefiniuj funkcję silnia, używając iteracji (pętli).
+
+# 2a. Zdefiniuj funkcję zwracającą największy element w liście - wersja iteracyjna.
+
+# 2b. Zdefiniuj funkcję zwracającą największy element w liście - wersja rekurencyjna.
+
+# 3a. Zdefiniuj funkcję odwracającą kolejność elementów w liście - wersja rekurencyjna.
+
+# 3b. Zdefiniuj funkcję odwracającą kolejność elementów w liście - wersja iteracyjna.
+
+# 4a. Zdefiniuj funkcję, która konkatenuje dwie listy - wersja rekurencyjna.
+
+# 4b. Zdefiniuj funkcję, która konkatenuje dwie listy - wersja iteracyjna.
+
+# 5a. Zdefiniuj funkcję, która wylicza największy wspólny dzielnik dwóch liczb - wersja rekurencyjna.
+
+# 5b. Zdefiniuj funkcję, która wylicza największy wspólny dzielnik dwóch liczb - wersja iteracyjna.
+    
+print("hello")    
