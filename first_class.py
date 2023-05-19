86e4942e00b75cd63a1a6ebfe8aeff162db6a83f
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2023-05-19 11:18:21 +0200

    Update first_class.py

--- first_class.py
+++ first_class.py
@@ -49,5 +49,27 @@ def factorial_iter(n):
 
 # 6a. Zdefiniuj funkcję, która wylicza największy wspólny dzielnik dwóch liczb - wersja rekurencyjna.
 # 6b. Zdefiniuj funkcję, która wylicza największy wspólny dzielnik dwóch liczb - wersja iteracyjna.
+
+# Sortowanie
+
+# 1a. Zdefinuj funkcję, która sortuje listę liczb - użyj rekurencji. Jeśli jest to potrzebne
+# zdefiniuj funkcje pomocnicze. 
+# 1b. Zdefinuj funkcję, które sortuje listę liczb - użyj iteracji.
+
+# 2. Zdefiniuj funkcję, która wstawi daną liczbę w odpowiednie miejsce posortowanej listy - użyj rekurencji.
+
+# 3a. Zdefiniuj funkcję, która sprawdzi ile razy dany element występuje na danej liście - użyj rekurencji.
+# 3b. Zdefiniuj funkcję, która sprawdzi ile razy dany element występuje na danej liście - użyj iteracji.
+
+# 4. Trójkąt Pacala. Zdefinuj funkcję, która dostaje dwa argumenty: numer kolumny i numer wiersza, zwraca zaś 
+# liczbą Pascala która na danej współrzędnej występuje. 
+# Działanie:
+# pacal(3,1) = 3 (3 jest liczbą pascala występującą w 3 wierszu 1 kolumny)
+# pascal(5,3) = 10 (10 jest liczbą pascala występującą w 5 wierszu 3 kolmuny)
+
+# 5. Zdefiniuj funkcję "balance", która sprawdza, czy w danym Stringu nawiasy są ustawione w 
+# prawidłowy sposób, tj. czy każde lewy nawias ma swój prawy nawias do pary i czy są one dobrze ustawione.
+# Użyj rekurencji ogonowej.
+
     
     
