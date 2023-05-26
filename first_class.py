001f2da77d279ce66e009d107f234dcd45a6f3c3
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2023-05-26 10:43:44 +0200

    Update first_class.py

--- first_class.py
+++ first_class.py
@@ -36,40 +36,87 @@ def factorial_iter(n):
         return wynik
 
 # 2a. Zdefiniuj funkcję zwracającą największy element w liście - wersja iteracyjna.
+def maximum_iter(list):
+    max = list[0]
+    for x in list:
+        if x > max:
+            max = x
+    return max
 # 2b. Zdefiniuj funkcję zwracającą największy element w liście - wersja rekurencyjna.
+def maximum_recur(L):
+    if len(L) == 1:
+        return L[0]
+    else:
+        if L[0] > maximum_recur(L[1:]):
+            return L[0]
+        else:
+            return maximum_recur(L[1:])
 
 # 3a. Zdefiniuj funkcję odwracającą kolejność elementów w liście - wersja rekurencyjna.
+def reverse_rec(list):
+    if len(list) == 0:
+        return []
+    else:
+        return reverse_rec(list[1:]) + [list[0]]
 # 3b. Zdefiniuj funkcję odwracającą kolejność elementów w liście - wersja iteracyjna.
+def reverse_iter(list):
+    start = []
+    for i in list:
+        start = [i] + start 
+    return start
 
 # 4a. Zdefiniuj funkcję, która konkatenuje dwie listy - wersja rekurencyjna.
+def concaten(list1, list2):
+    if len(list1) == 0:
+        return list2
+    else:
+        return [list1[0]] + concaten(list1[1:], list2)
 # 4b. Zdefiniuj funkcję, która konkatenuje dwie listy - wersja iteracyjna.
+def concaten_iter(list1, list2):
+    if len(list2) == 0:
+        return list1
+    else:
+        total = list1
+        for i in list2:
+           total = total + [i]
+        return total
 
 # 5a. Zdefinuj funkcję, która sprawdza, czy dany element znajduje się na liście - wersja rekurencyjna.
+def check_list(elem, list):
+    if len(list)==0:
+        return False
+    else:
+        if elem == list[0]:
+            return True
+        else:
+            return check_list(elem,list[1:])
 # 5b. Zdefinuj funkcję, która sprawdza, czy dany element znajduje się na liście - wersja iteracyjna.
+def check_iter(elem, list):
+    if list == []:
+        return False
+    else:
+        temp = False
+        for i in list:
+            if i == elem:
+                temp = True
+        return temp
 
 # 6a. Zdefiniuj funkcję, która wylicza największy wspólny dzielnik dwóch liczb - wersja rekurencyjna.
+def gcd(a, b):
+    if a == 0:
+        return b
+    else:
+        return gcd(b % a, a)
 # 6b. Zdefiniuj funkcję, która wylicza największy wspólny dzielnik dwóch liczb - wersja iteracyjna.
+def gcd_iter(a, b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
 
-# Sortowanie
 
-# 1a. Zdefinuj funkcję, która sortuje listę liczb - użyj rekurencji. Jeśli jest to potrzebne
-# zdefiniuj funkcje pomocnicze. 
-# 1b. Zdefinuj funkcję, które sortuje listę liczb - użyj iteracji.
-
-# 2. Zdefiniuj funkcję, która wstawi daną liczbę w odpowiednie miejsce posortowanej listy - użyj rekurencji.
-
-# 3a. Zdefiniuj funkcję, która sprawdzi ile razy dany element występuje na danej liście - użyj rekurencji.
-# 3b. Zdefiniuj funkcję, która sprawdzi ile razy dany element występuje na danej liście - użyj iteracji.
-
-# 4. Trójkąt Pacala. Zdefinuj funkcję, która dostaje dwa argumenty: numer kolumny i numer wiersza, zwraca zaś 
-# liczbą Pascala która na danej współrzędnej występuje. 
-# Działanie:
-# pacal(3,1) = 3 (3 jest liczbą pascala występującą w 3 wierszu 1 kolumny)
-# pascal(5,3) = 10 (10 jest liczbą pascala występującą w 5 wierszu 3 kolmuny)
-
-# 5. Zdefiniuj funkcję "balance", która sprawdza, czy w danym Stringu nawiasy są ustawione w 
-# prawidłowy sposób, tj. czy każde lewy nawias ma swój prawy nawias do pary i czy są one dobrze ustawione.
-# Użyj rekurencji ogonowej.
 
     
     
