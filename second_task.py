183acbc597c3195a7a75aba6a215e847fd4880f2
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2023-05-26 14:35:28 +0200

    Update second_task.py

--- second_task.py
+++ second_task.py
@@ -1,4 +1,10 @@
-
+# Zdefiniuj funkcję "apply", która bierze funkcję "f" oraz listę "list" 
+# obiektów a następnie zwraca listę, której elementy są wartościam 
+# funkcji "f" dla argumentów z listy "list". Użyj rekurencji!!!
+# Działanie:
+# my_list = list(range(10))
+# function = lambda x: x*2
+# apply(function, my_list) = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
 
 
 
