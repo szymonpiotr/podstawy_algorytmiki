099fa4b1f8210b1a526ca54a031c9b1ad611ec4f
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2023-05-26 12:27:08 +0200

    Update second_class.py

--- second_class.py
+++ second_class.py
@@ -2,7 +2,24 @@
 
 # 1a. Zdefinuj funkcję, która sortuje listę liczb - użyj rekurencji. Jeśli jest to potrzebne
 # zdefiniuj funkcje pomocnicze. 
+def recurSortt(list):
+    if list == []:
+        return []
+    else:
+        pivot = random.choice(list)
+        smaller = [i for i in list if i < pivot]
+        larger = [i for i in list if i > pivot]
+        return recurSortt(smaller) + [pivot] + recurSortt(larger)
 # 1b. Zdefinuj funkcję, które sortuje listę liczb - użyj iteracji.
+def iterSort(list):
+    for step in range(1, len(list)):
+        key = list[step]
+        j = step - 1      
+        while j >= 0 and key < list[j]:
+            list[j + 1] = list[j]
+            j = j - 1
+        list[j + 1] = key
+    return list
 
 # 2. Zdefiniuj funkcję, która wstawi daną liczbę w odpowiednie miejsce posortowanej listy - użyj rekurencji.
 
