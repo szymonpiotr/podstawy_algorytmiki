2e8bee458e281276c292bb29bfed0f1d47a6f896
szymonpiotr <33660379+szymonpiotr@users.noreply.github.com>
2023-06-15 09:02:47 +0200

    homework.py

---
+++ homework.py
@@ -0,0 +1,3 @@
+# Powiemy, że drzewo jest w równowadze wtedy i tylko wtedy, ilość liści w lewym poddrzewie korzenia różni się
+# co najwyżej o 1 od liczby liści w prawym poddrzewie korzenia. Zdefiniuj funkcję "stable", któa sprawdza, czy dane 
+# drzewo jest w równowadze.  
